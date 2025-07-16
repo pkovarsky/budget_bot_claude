e063dd54933cd0bd3dd013bf79b37e29701ba541
peterkovarsky <pkovarsky1993@gmail.com>
2025-07-16 12:43:06 +0100

    a lot of updates :)

---
+++ scripts/__init__.py
@@ -0,0 +1,9 @@
+"""
+Scripts package for Budget Bot
+
+This package contains utility and deployment scripts:
+- Migration scripts
+- Database management scripts  
+- Bot management scripts
+- Deployment automation scripts
+"""
